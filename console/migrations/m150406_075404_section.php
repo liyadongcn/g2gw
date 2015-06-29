b7062864889ad8e4819ca771311bd10ea1d0bdcb
liyadongcn <liyadongcn@gmail.com>
2015-06-29 17:13:50 +0800

    控制台应用增加到仓库

---
+++ console/migrations/m150406_075404_section.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150406_075404_section extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m150406_075404_section cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
